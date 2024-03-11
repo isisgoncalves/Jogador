e1bf3cc991bc083bfff294fa5d83abc3612d5c8e
isisgoncalves <isisvow@gmail.com>
2024-03-11 07:36:13 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿class Program
+{
+    static void Main()
+    {
+        string nome = Console.ReadLine();
+
+        string nacionalidade = Console.ReadLine();
+
+        int idade = int.Parse(Console.ReadLine());
+
+        string posicao = Console.ReadLine();
+        
+        Jogador jogador = new Jogador(nome, nacionalidade, idade, posicao);
+        
+        Console.WriteLine("Jogador criado!");
+        Console.WriteLine(jogador.Nome);
+        Console.WriteLine(jogador.Nacionalidade);
+        Console.WriteLine(jogador.Idade);
+        Console.WriteLine($"Posição: {jogador.Posicao}");
+    }
+}
