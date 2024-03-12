2a841abf27a528769771dabc702ebf0dcb01651f
isisgoncalves <isisvow@gmail.com>
2024-03-12 10:04:33 -0300

    Update Jogador.cs

--- Jogador.cs
+++ Jogador.cs
@@ -1,12 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Jogador
+class Jogador
 {
-    public class Jogador
+    public string Nome { get; set; }
+    public string Nacionalidade { get; set; }
+    public int Idade { get; set; }
+    public string Posicao { get; set; }
+
+    public Jogador(string nome, string nacionalidade, int idade, string posicao)
     {
-        
+        Nome = nome;
+        Nacionalidade = nacionalidade;
+        Idade = idade;
+        Posicao = posicao;
     }
 }
